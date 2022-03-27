5ad85ab6d287297bc3a33172cb68729661a0be4d
Colin Dunn <colin@ritual.com>
2022-03-27 09:27:32 -0700

    Initial Commit

---
+++ TideMachineApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TideMachineApp
+//
+//  Created by Colin Dunn on 3/27/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
